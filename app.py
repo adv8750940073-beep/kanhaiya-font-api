c3657d9dc4517813f9ecf14fd431677fd915a59c
Kanhaiya <adv8750940073@gmail.com>
2026-01-06 23:50:21 +0530

    Initial KrutiDev Unicode Font API

---
+++ app.py
@@ -0,0 +1,52 @@
+from fastapi import UploadFile, File
+import pdfplumber
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+# Import converter
+from Unicode_KrutiDev_converter import KrutiDev_to_Unicode
+
+app = FastAPI(title="KrutiDev to Unicode API")
+
+# CORS (browser se call allow karne ke liye)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class InputText(BaseModel):
+    text: str
+
+@app.get("/")
+def home():
+    return {
+        "status": "OK",
+        "message": "KrutiDev to Unicode API is running"
+    }
+
+@app.post("/convert")
+def convert(data: InputText):
+    result = KrutiDev_to_Unicode(data.text)
+    return {"unicode": result}
+
+@app.post("/convert-pdf")
+async def convert_pdf(file: UploadFile = File(...)):
+    text = ""
+
+    with pdfplumber.open(file.file) as pdf:
+        for page in pdf.pages:
+            page_text = page.extract_text()
+            if page_text:
+                text += page_text + "\n"
+
+    if not text.strip():
+        return {"error": "PDF se text extract nahi hua (scanned PDF ho sakta hai)"}
+
+    unicode_text = KrutiDev_to_Unicode(text)
+
+    return {
+        "unicode": unicode_text
+    }
